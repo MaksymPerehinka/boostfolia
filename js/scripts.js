3382ab3f8092cc7187aecdb788fbf93d21ce89d7
MaximPereginka <maxim.pereginka@outlook.com>
2016-09-04 00:20:47 +0300

    add portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,22 @@
  * Created by Maxim on 03.09.2016.
  */
 
+function toggle_portfolio(id) {
+    $('.selection').fadeOut('slow');
+    $("#" + id).fadeIn("slow");
+    $(".button-list a").removeClass("active");
+    $("." + id).toggleClass("active");
+    return false;
+}
+
 $(document).ready(function(){
+    toggle_portfolio("all-projects");
+
+    $('.play-btn button').on('click', function(){
+        document.getElementById('video').play();
+        $('.video-container .overlay').fadeOut('slow');
+    });
+
     $('.video-container .overlay').on('click', function(){
         document.getElementById('video').play();
         $('.video-container .overlay').fadeOut('slow');
